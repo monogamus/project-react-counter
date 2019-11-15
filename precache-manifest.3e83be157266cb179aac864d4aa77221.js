1762320d3bc11aa3b1501ba9857b2727599d3428
monogamus <monogamus@yahoo.com>
2019-11-15 15:59:38 +0700

    Updates

---
+++ precache-manifest.3e83be157266cb179aac864d4aa77221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dae19c5e9a4f33feb6f7e33a007b5c9",
+    "url": "/project-react-counter/index.html"
+  },
+  {
+    "revision": "86dcb3e69ea916e6349f",
+    "url": "/project-react-counter/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "19524606ce35a40ce584",
+    "url": "/project-react-counter/static/js/2.65692ebd.chunk.js"
+  },
+  {
+    "revision": "86dcb3e69ea916e6349f",
+    "url": "/project-react-counter/static/js/main.f9dcf592.chunk.js"
+  },
+  {
+    "revision": "139c00d0b15bcedd0500",
+    "url": "/project-react-counter/static/js/runtime-main.e7cb0afc.js"
+  },
+  {
+    "revision": "2baccc669b1304e9d24fd3e9a5df17f1",
+    "url": "/project-react-counter/static/media/confederate_p.2baccc66.jpg"
+  }
+]);
